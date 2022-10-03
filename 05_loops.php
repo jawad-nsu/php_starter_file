3d46e84bf142978308b3392869c9c82c6d152934
jawad-nsu <jawadurrahim1997@gmail.com>
2022-10-03 14:52:15 +0600

    added loops

---
+++ 05_loops.php
@@ -0,0 +1,24 @@
+<?php
+
+$posts = ['First Post', 'Second Post', 'Third Post'];
+
+// foreach($posts as $index => $post){
+//     echo $index+1 . ' - ' . $post . '<br>';
+// }
+
+$person = [
+    'first name' => 'Jawad',
+    'last name' => 'Rahim',
+    'email' => 'jawad@gmail.com'
+];
+
+foreach($person as $key => $value){
+    echo $key . ' - ' . $value . '<br>';
+}
+
+
+
+
+
+
+?>
